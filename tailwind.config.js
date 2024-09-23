a156fb8fd641e2077115e761cf3605509b99c944
mohammad8549 <mohammad1385.naderiii@gmail.com>
2024-09-23 21:10:56 +0330

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'faNum': ['"faNum"'],
+      'fa': ['"Shabnam"'],
+      'sahel': ['sahel'],
+    },
   },
   plugins: [],
 }
